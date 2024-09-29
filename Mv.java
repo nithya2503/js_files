8a6a27d7d57f1b08d188ee10458b71bc71c67443
nithyasri <kattelanithyasri9100@gmail.com>
2024-09-28 20:06:01 -0400

    added java file

---
+++ Mv.java
@@ -0,0 +1,12 @@
+public class Mv {
+    public void ver() {
+        System.out.println("version 1");
+    }
+
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        Mv m = new Mv();
+        m.ver();
+    }
+}
